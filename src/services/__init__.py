aced20b71de8b619da07cd8299e465872ecd5392
Mo Abdulhussain <abdulhu6@gmail.com>
2025-10-21 00:33:36 -0400

    chore: update configuration and module exports for new services

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,5 +1,9 @@
 """Services package for business logic and integrations"""
 
 from .bill_integration_service import BillIntegrationService
+from .embedding_service import EmbeddingService
 
-__all__ = ["BillIntegrationService"]
+__all__ = [
+    "BillIntegrationService",
+    "EmbeddingService",
+]
